24116db2d06ccf17b7f2aed94adbafac4268530a
K1llM@n <k1llmanws@yandex.ru>
2020-07-13 20:20:10 +0300

    Исправлено некорректное отображение положения виджетов в полноэкранном режиме.

--- Yandex.Dj/Program.cs
+++ Yandex.Dj/Program.cs
@@ -12,6 +12,7 @@ namespace Yandex.Dj
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000");
     }
 }
